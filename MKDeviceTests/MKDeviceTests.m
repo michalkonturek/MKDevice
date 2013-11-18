9262dd5fb4d9d36a307c24206f30358e109dcdd8
Michal Konturek <michal.konturek@gmail.com>
2013-11-18 13:46:43 +0000

    first commit

---
+++ MKDeviceTests/MKDeviceTests.m
@@ -0,0 +1,34 @@
+//
+//  MKDeviceTests.m
+//  MKDeviceTests
+//
+//  Created by Michal Konturek on 18/11/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MKDeviceTests : XCTestCase
+
+@end
+
+@implementation MKDeviceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
