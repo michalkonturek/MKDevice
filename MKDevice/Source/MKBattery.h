fea606e15c6f9c6adfa76bbd28cb57e5edadcd7e
Michal Konturek <michal.konturek@gmail.com>
2013-11-28 22:17:08 +0000

    .

---
+++ MKDevice/Source/MKBattery.h
@@ -0,0 +1,13 @@
+//
+//  MKBattery.h
+//  MKDevice
+//
+//  Created by Michal Konturek on 28/11/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKBattery : NSObject
+
+@end
---
+++ MKDevice/Source/MKBattery.m
@@ -0,0 +1,13 @@
+//
+//  MKBattery.m
+//  MKDevice
+//
+//  Created by Michal Konturek on 28/11/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import "MKBattery.h"
+
+@implementation MKBattery
+
+@end
