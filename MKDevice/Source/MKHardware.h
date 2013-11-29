bfc54cbe667cd0e442e774da955d0e5f77a2a9cf
Michal Konturek <michal.konturek@gmail.com>
2013-11-29 18:38:11 +0000

    Added MKHardware.

---
+++ MKDevice/Source/MKHardware.h
@@ -0,0 +1,13 @@
+//
+//  MKHardware.h
+//  MKDevice
+//
+//  Created by Michal Konturek on 29/11/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKHardware : NSObject
+
+@end
---
+++ MKDevice/Source/MKHardware.m
@@ -0,0 +1,13 @@
+//
+//  MKHardware.m
+//  MKDevice
+//
+//  Created by Michal Konturek on 29/11/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import "MKHardware.h"
+
+@implementation MKHardware
+
+@end
