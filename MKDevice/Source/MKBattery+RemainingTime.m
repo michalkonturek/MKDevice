377c2e0607b2354b52bcfc615377c7ab329292f6
Michal Konturek <michal.konturek@gmail.com>
2013-11-29 18:10:18 +0000

    .

---
+++ MKDevice/Source/MKBattery+RemainingTime.m
@@ -0,0 +1,13 @@
+//
+//  MKBattery+RemainingTime.m
+//  MKDevice
+//
+//  Created by Michal Konturek on 29/11/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import "MKBattery+RemainingTime.h"
+
+@implementation MKBattery (RemainingTime)
+
+@end
